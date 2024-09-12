1b3cecc3ec3f7a1ae56acac674ec649f3c3a1b78
MuhammadPrasetyoN <muhammad.prasetyo.2005356@students.um.ac.id>
2024-09-12 11:29:09 +0700

    welcome email,  login logout routes, user routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 
 export const signup = async(req, res) => {
     try {
@@ -46,18 +47,64 @@ export const signup = async(req, res) => {
 
         res.status(201).json({ message: "user registered successfully" });
 
+        const profileUrl = process.env.CLIENT_URL + "/profile/" + user.username;
         // todo: send welcome email
+
+        try {
+            await sendWelcomeEmail(user.email, user.name, profileUrl);
+        } catch (emailError) {
+            console.log("Error sending welcome email", emailError);
+        }
     } catch (error) {
         console.log("Error in signup:", error.message);
         res.status(500).json({message: "Internal server error"})
     }
 }
 
-export const login = (req, res) => {
-    res.send("login");
+export const login = async(req, res) => {
+    try {
+        const {username,password} = req.body;
+        
+        //check if user exists
+        const user = await User.findOne({username});
+        if(!user){
+            return res.status(400).json({ message: "User does not exist" });
+        }
+
+        //check password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({ message: "Incorrect password" });
+        }
+        
+        //create a token and send it
+        const token = jwt.sign({userId:user._id}, process.env.JWT_SECRET, {expiresIn:"3d"})
+        await res.cookie("jwt-linkedin", token, {
+            httpOnly:true, // prevent XSS attack
+            maxAge: 3 * 24 * 60 * 60 * 1000,
+            sameSite: "strict", // prevent CSRF attack
+            secure: process.env.NODE_ENV === "production", // prevents man-in-the-middle attacks
+        }) //3 days
+
+        res.json({ message: "logged in successfully" });
+    } catch (error) {
+        console.log("Error in login", error.message);
+        res.status(500).json({message: "Server error"})
+    }
 }
 
 
 export const logout = (req, res) => {
-    res.send("logout");
+    res.clearCookie("jwt-linkedin");
+    res.json({message: "Logged Out successfully"});
+}
+
+
+export const getCurrentUser = async (req, res) => {
+    try {
+        res.json(req.user);
+    } catch (error) {
+        console.error("Error in getCurrentUser controller:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -10,8 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // parse JSON request bodies
+app.use(cookieParser());
+
 
 app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/users", userRoutes)
 
 app.listen(PORT, () =>{
     console.log(`Server Running on port ${PORT}`);
